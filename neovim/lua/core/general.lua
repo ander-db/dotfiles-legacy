f0c52a3c359ec57d9953fee9ae8158df2c7df05a
Ander <adb35@alu.ua.es>
2022-11-06 00:38:25 +0100

    Indentation

--- neovim/lua/core/general.lua
+++ neovim/lua/core/general.lua
@@ -1,20 +1,20 @@
 local opt = vim.opt
 local cache_dir = vim.env.HOME .. '/.cache/nvim/'
 
-vim.g.mapleader     = ' ';
-opt.relativenumber  = true
-opt.number          = true
-opt.tabstop         = 2
-opt.shiftwidth      = 2
-opt.expandtab       = true
-opt.smarttab        = true
-opt.autoindent      = true
-opt.showtabline     = 2
-opt.cursorline      = true
-opt.splitbelow      = true
-opt.splitright      = true
-opt.wrap            = true
-opt.clipboard       = 'unnamedplus'
+vim.g.mapleader    = ' ';
+opt.relativenumber = true
+opt.number         = true
+opt.tabstop        = 2
+opt.shiftwidth     = 2
+opt.expandtab      = true
+opt.smarttab       = true
+opt.autoindent     = true
+opt.showtabline    = 2
+opt.cursorline     = true
+opt.splitbelow     = true
+opt.splitright     = true
+opt.wrap           = true
+opt.clipboard      = 'unnamedplus'
 
 opt.iskeyword:append("-")
 
@@ -24,4 +24,3 @@ opt.undodir = cache_dir .. 'undo/'
 opt.backupdir = cache_dir .. 'backup/'
 opt.viewdir = cache_dir .. 'view/'
 opt.spellfile = cache_dir .. 'spell/en.uft-8.add'
-
