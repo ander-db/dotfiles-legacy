17cb09f8a7272298afdf031e8087839588e693fc
Ander <adb35@alu.ua.es>
2022-11-13 14:18:40 +0100

    Which key updateg

--- neovim/lua/_which-key.lua
+++ neovim/lua/_which-key.lua
@@ -47,7 +47,7 @@ require('which-key').setup {
   },
   layout = {
     height = { min = 4, max = 20 }, -- min and max height of the columns
-    width = { min = 20, max = 25 }, -- min and max width of the columns
+    width = { min = 20, max = 30 }, -- min and max width of the columns
     spacing = 2, -- spacing between columns
     align = "center", -- align columns left, center or right
   },
