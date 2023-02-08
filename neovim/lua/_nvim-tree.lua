97d65884836ad427986e3862c5a6d5189c7dea26
Ander <adb35@alu.ua.es>
2023-02-08 14:14:00 +0100

    Nvim-tree folder

--- neovim/lua/_nvim-tree.lua
+++ neovim/lua/_nvim-tree.lua
@@ -5,6 +5,7 @@ vim.g.lua_tree_auto_close = 1
 
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
+  --open_on_setup = true,
   view = {
     adaptive_size = true,
     side = "left",
@@ -40,3 +41,27 @@ require("nvim-tree").setup({
 
 
 map("n", "<C-t>", ":NvimTreeToggle<CR>", { silent = true })
+
+
+local function open_nvim_tree(data)
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
